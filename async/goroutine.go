d5c9e16ee38499d7607a420652366ba8e23d5ad5
Edwin Xu <edwinhuish@gmail.com>
2024-12-17 16:53:59 +0800

    优化代码

--- async/goroutine.go
+++ async/goroutine.go
@@ -14,32 +14,35 @@ import (
 func Do(task func(), finally ...func(err error)) {
 
 	go func() {
+
+		var taskErr error = nil
+
+		var _finally func(err error)
+		// 根据是否提供了错误处理函数来决定使用哪一个。
+		if len(finally) > 0 {
+			_finally = finally[0]
+		} else {
+			// 默认的错误处理函数，将错误记录到日志。
+			_finally = func(err error) {
+				if err != nil {
+					log.Printf("async error: %v\n", err)
+				}
+			}
+		}
+
+		defer _finally(taskErr)
+
 		defer func() {
 			// 检查是否有panic发生，如果有，则根据情况调用错误处理函数。
 			if err := recover(); err != nil {
 
-				var errHandler func(err error)
-				// 根据是否提供了错误处理函数来决定使用哪一个。
-				if len(finally) > 0 {
-					errHandler = finally[0]
-				} else {
-					// 默认的错误处理函数，将错误记录到日志。
-					errHandler = func(err error) {
-						log.Printf("async error: %v\n", err)
-					}
-				}
-
 				// 根据recover返回的错误类型，调用错误处理函数。
 				switch v := err.(type) {
 				case error:
-					errHandler(v)
+					taskErr = v
 				default:
-					// 如果不是error类型，创建一个error类型并调用错误处理函数。
-					errHandler(fmt.Errorf("%+v", v))
-				}
-			} else {
-				if len(finally) > 0 {
-					finally[0](nil)
+					// 如果不是error类型，创建一个error类型
+					taskErr = (fmt.Errorf("%+v", v))
 				}
 			}
 		}()
